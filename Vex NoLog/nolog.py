6b8f625e5f7c5e4c7b0e61fbf551e74515cad3f4
dex <97322316+im-dexx@users.noreply.github.com>
2022-03-25 02:28:49 -0400

    releasing fix soon

--- Vex NoLog/nolog.py
+++ Vex NoLog/nolog.py
@@ -15,32 +15,5 @@ import os
 
 
 # NoLog ===========|
-def scanner():
-    file = input("File Path: ")
 
-    try:
-        scan = open(file, "r")
-    except:
-        print("error, please try again.")
-        scanner()
-
-    for line in scan:
-        checkip1 = line.split("ip")
-
-        checkwebhook1 = line.split("webhook")
-        checkwebhook2 = line.split("discord")
-
-        checklink1 = line.split("https")
-
-        for x in checkip1:
-            print("IP Logger found: looked for 'ip'")
-        for x in checkwebhook1:
-            print("Webhook found: looked for 'webhook'")
-        for x in checkwebhook2:
-            print("Webhook found: looked for 'discord'")
-        for x in checklink1:
-            print("Link found: looked for 'https'")
-
-    print("\n")
-    scanner()
-scanner()
+# lol sorry program is bugged
